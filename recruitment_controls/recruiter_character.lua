a8d210490cf577b15aafa6e5c8fce393f77f2641
DFlunchbreak <matte901@gmail.com>
2018-06-04 15:31:03 -0400

    Methods Updated: Added Logging to multiple Added AddtoQueue Added Blank RemoveFromQueue Added Blank EvaluateArmy

--- recruitment_controls/recruiter_character.lua
+++ recruitment_controls/recruiter_character.lua
@@ -58,17 +58,32 @@ end
 
 --v function(self: RECRUITER_CHARACTER, manager: RECRUITER_MANAGER)
 function RecruiterCharacter.SetManager(self, manager)
+    RCLOG("Set the Manager for a RECRUITER_CHARACTER with CQI ["..tostring(self.cqi).."]", "RecruiterCharacter.SetManager(self, manager)")
     self.RecruiterManager = manager
 end
 
 
 --v function(self: RECRUITER_CHARACTER) --> boolean
 function RecruiterCharacter.IsQueueEmpty(self)
+    RCLOG("Queue Empty returning ["..tostring(self.QueueEmpty).."] for a RECRUITER_CHARACTER with CQI ["..tostring(self.cqi).."]", "RecruiterCharacter.IsQueueEmpty(self)")
     return self.QueueEmpty
 end
 
+--v function(self: RECRUITER_CHARACTER, unit_component_ID: string)
+function RecruiterCharacter.AddToQueue(self, unit_component_ID)
+    table.insert(self.QueueTable, unit_component_ID)
+end
+
+--v function(self: RECRUITER_CHARACTER, queue_component_ID: string)
+function RecruiterCharacter.RemoveFromQueue(self, queue_component_ID)
+
+end
+
+
+
 --v function(self: RECRUITER_CHARACTER)
 function RecruiterCharacter.EmptyQueue(self)
+    RCLOG("Emptying the Queue for a RECRUITER_CHARACTER with CQI ["..tostring(self.cqi).."]", "RecruiterCharacter.EmptyQueue(self)")
     self.QueueEmpty = true;
     self.QueueTable = {}
 end
@@ -77,11 +92,18 @@ end
 function RecruiterCharacter.SetRegion(self)
     local character = cm:get_character_by_cqi(self.cqi)
     self.RegionKey = character:region():name()
+    RCLOG("Set Region to ["..self.RegionKey.."] a RECRUITER_CHARACTER with CQI ["..tostring(self.cqi).."]", "RecruiterCharacter.SetRegion(self)" )
 end
 
 --v function(self: RECRUITER_CHARACTER) --> string
 function RecruiterCharacter.GetRegion(self)
+    RCLOG("Retrieved the Region ["..self.RegionKey.."] from a RECRUITER_CHARACTER with CQI ["..tostring(self.cqi).."]", "RecruiterCharacter.GetRegion(self)")
     return self.RegionKey
+end
+
+--v function(self: RECRUITER_CHARACTER)
+function RecruiterCharacter.EvaluateArmy(self)
+    local army = cm:get_character_by_cqi(self.cqi):military_force()
 end
 
 
