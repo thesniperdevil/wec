dd2f7e35c4d939acca6ba8bc29e052103ad4fec8
chadvandy <chadvandewettering@gmail.com>
2018-06-23 20:05:48 -0400

    added the AI performed event listener

--- wec_rites.lua
+++ wec_rites.lua
@@ -215,7 +215,75 @@ function OWR_chs_cults()
 	for i = 1, #majorcities_list do
 		cm:apply_effect_bundle_to_region("wh_main_ritual_chs_cults_corruption", majorcities_list[i], 10)
 	end;
+
 end;
+
+
+
+function OWR_ai_rite_performed_event(faction, ritual_key)
+	if faction:has_home_region() then
+		local capital = faction:home_region():settlement();
+		local factions_known = faction:factions_met();
+		local culture = faction:culture();
+		local id = 820;
+		
+		
+		for i = 0, factions_known:num_items() - 1 do
+			local current_faction = factions_known:item_at(i);
+			
+			if current_faction:is_human() then
+				local current_faction_name = current_faction:name();
+				local primary_detail = "event_feed_strings_text_wh2_event_feed_string_scripted_event_ai_rite_performed_primary_detail_" .. ritual_key;
+				local ai_faction_name = "factions_screen_name_" .. faction:name()
+				local secondary_detail = "event_feed_strings_text_wh2_event_feed_string_scripted_event_ai_rite_performed_secondary_detail_" .. ritual_key;
+				
+				cm:show_message_event_located(
+					current_faction_name,
+					primary_detail,
+					ai_faction_name,
+					secondary_detail,
+					capital:logical_position_x(),
+					capital:logical_position_y(),
+					true,
+					id
+				);
+				
+			end;
+		end;
+		
+	elseif wh_faction_is_horde(faction) then
+		local factions_known = faction:factions_met();
+		local culture = faction:culture();
+		local id = 820;
+		
+		
+		for i = 0, factions_known:num_items() - 1 do
+			local current_faction = factions_known:item_at(i);
+			
+			if current_faction:is_human() then
+				local current_faction_name = current_faction:name();
+				local primary_detail = "event_feed_strings_text_wh2_event_feed_string_scripted_event_ai_rite_performed_primary_detail_" .. ritual_key;
+				local ai_faction_name = "factions_screen_name_" .. faction:name()
+				local secondary_detail = "event_feed_strings_text_wh2_event_feed_string_scripted_event_ai_rite_performed_secondary_detail_" .. ritual_key;
+				
+				cm:show_message_event(
+					current_faction_name,
+					primary_detail,
+					ai_faction_name,
+					secondary_detail,
+					true,
+					id
+				);
+				
+			end;
+		end;
+			
+	
+	end;
+end;
+
+
+
 
 
 
@@ -1783,8 +1851,15 @@ core:add_listener(
 		local faction = context:performing_faction();
 		
 		if ritual_key == "wh_main_ritual_chs_cults" then
-			OWR_chs_cults()
+			OWR_chs_cults();
 		end;
+		
+		local is_human = faction:is_human();
+		
+		if not is_human and (ritual_key == "wh_main_ritual_chs_cults" or ritual_key == "wh_main_ritual_ksl_winter" or ritual_key == "wh_main_ritual_vmp_intrigue" or ritual_key == "wh_main_ritual_brt_errantry") then
+			OWR_ai_rite_performed_event(faction, ritual_key);
+		end; 
+
 	end,
 	true);
 		
@@ -1793,10 +1868,6 @@ core:add_listener(
 	
 	
 end;
-
-
-
-
 
 
 
@@ -1814,5 +1885,5 @@ function wec_rites()
 	vandy_scripted_effects();
 	
 	OWRLOG("OWR INIT COMPLETE");
-	end;
+end;
 	
