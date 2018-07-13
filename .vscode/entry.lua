72da65b959fe30cdb1b558b1bfd7b72229ed2f8b
chadvandy <chadvandewettering@gmail.com>
2018-07-12 20:55:28 -0400

    Added CA Types for BM; introduced emil_qb battle_script.lua

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -9,3 +9,4 @@ require("dev_tools/!!wec_development_tools")
 require("exterior_scripting/export_helpers__faction_replacement")
 --require("le/wec_loreful_empires")
 
+require("emil_qb/battle_script");
