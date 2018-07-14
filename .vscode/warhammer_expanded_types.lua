87cd8a897778b34d231247a897eb090f37e7635d
DrunkFlamingo <matte901@gmail.com>
2018-07-14 15:24:37 -0400

    initial_mod_config

--- .vscode/warhammer_expanded_types.lua
+++ .vscode/warhammer_expanded_types.lua
@@ -22,3 +22,10 @@
 --# region: string, forename: string, surname: string, subtype: string, army: string, x: number, y: number, is_faction_leader: boolean
 --#}
 --# assume global class DEV_TOOL_MANAGER
+
+
+--# assume global class MOD_CONFIGURATION_MANAGER
+--# assume global class MCM_MOD
+--# assume global class MCM_SETTING
+--# assume global class MCM_VAR
+--# assume global class MCM_CHOICE
