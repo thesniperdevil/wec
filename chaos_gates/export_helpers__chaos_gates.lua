2f49aaf5686c1c1934ed16b35ac6bcc420d3a6de
DrunkFlamingo <matte901@gmail.com>
2018-08-15 17:51:09 -0400

    this locations shit

--- chaos_gates/export_helpers__chaos_gates.lua
+++ chaos_gates/export_helpers__chaos_gates.lua
@@ -34,7 +34,167 @@ DF_CHAOS_ARMY_LIST =  {"wh_dlc01_chs_inf_forsaken_0", "wh_main_chs_mon_chaos_war
 DF_CHAOS_ARMY_SIZES = {9, 11, 13} --:vector<number>
 DF_CHAOS_ARMY_FACTION = "wh2_main_chs_chaos_incursion_lzd" --:string
 
-
+DF_SPAWN_LOCATIONS = {
+    ["wh_main_goromandy_mountains_baersonlings_camp"] = {762,594}, 
+    ["wh_main_eastern_oblast_volksgrad"] = {724,584}, 
+    ["wh_main_eastern_oblast_praag"] = {699,585}, 
+    ["wh_main_goromandy_mountains_frozen_landing"] = {734,624}, 
+    ["wh_main_gianthome_mountains_sjoktraken"] = {691,637}, 
+    ["wh_main_gianthome_mountains_khazid_bordkarag"] = {657,640}, 
+    ["wh_main_gianthome_mountains_kraka_drak"] = {685,656}, 
+    ["wh2_main_hell_pit_hell_pit"] = {675,611}, 
+    ["wh_main_northern_oblast_fort_ostrosk"] = {658,595}, 
+    ["wh_main_northern_oblast_fort_straghov"] = {636,622}, 
+    ["wh_main_trollheim_mountains_the_tower_of_khrakk"] = {601,625}, 
+    ["wh_main_trollheim_mountains_bay_of_blades"] = {545,625}, 
+    ["wh_main_trollheim_mountains_sarl_encampment"] = {550,651}, 
+    ["wh_main_mountains_of_hel_aeslings_conclave"] = {597,662}, 
+    ["wh_main_mountains_of_hel_altar_of_spawns"] = {575,699}, 
+    ["wh_main_mountains_of_hel_winter_pyre"] = {637,674}, 
+    ["wh_main_mountains_of_naglfari_varg_camp"] = {488,686}, 
+    ["wh_main_mountains_of_naglfari_naglfari_plain"] = {495,647}, 
+    ["wh_main_helspire_mountains_graeling_moot"] = {477,641}, 
+    ["wh_main_helspire_mountains_the_monolith_of_katam"] = {428,673}, 
+    ["wh_main_helspire_mountains_serpent_jetty"] = {394,660}, 
+    ["wh_main_vanaheim_mountains_troll_fjord"] = {361,609}, 
+    ["wh_main_vanaheim_mountains_bjornlings_gathering"] = {427,616}, 
+    ["wh_main_vanaheim_mountains_pack_ice_bay"] = {426,575}, 
+    ["wh_main_ice_tooth_mountains_icedrake_fjord"] = {467,591}, 
+    ["wh_main_ice_tooth_mountains_longship_graveyard"] = {504,597}, 
+    ["wh_main_ice_tooth_mountains_doomkeep"] = {517,649}, 
+    ["wh_main_troll_country_zoishenk"] = {612,599}, 
+    ["wh_main_troll_country_erengrad"] = {609,584}, 
+    ["wh_main_southern_oblast_zavastra"] = {649,551}, 
+    ["wh_main_southern_oblast_kislev"] = {684,544}, 
+    ["wh_main_southern_oblast_fort_jakova"] = {704,508}, 
+    ["wh_main_ostermark_bechafen"] = {655,525}, 
+    ["wh_main_ostermark_essen"] = {658,476}, 
+    ["wh_main_ostland_castle_von_rauken"] = {620,548}, 
+    ["wh_main_ostland_wolfenburg"] = {602,538}, 
+    ["wh_main_ostland_norden"] = {571,576}, 
+    ["wh_main_nordland_salzenmund"] = {536,569}, 
+    ["wh_main_nordland_dietershafen"] = {495,562}, 
+    ["wh_main_middenland_middenheim"] = {521,531}, 
+    ["wh_main_middenland_weismund"] = {498,500}, 
+    ["wh_main_middenland_carroburg"] = {487,466}, 
+    ["wh_main_hochland_brass_keep"] = {565,538}, 
+    ["wh_main_hochland_hergig"] = {580,516}, 
+    ["wh_main_talabecland_kemperbad"] = {534,454}, 
+    ["wh_main_talabecland_talabheim"] = {569,497}, 
+    ["wh_main_stirland_wurtbad"] = {584,441}, 
+    ["wh_main_stirland_the_moot"] = {611,409}, 
+    ["wh_main_averland_grenzstadt"] = {612,387}, 
+    ["wh_main_eastern_sylvania_castle_drakenhof"] = {680,420}, 
+    ["wh_main_eastern_sylvania_eschen"] = {678,445}, 
+    ["wh_main_eastern_sylvania_waldenhof"] = {684,458}, 
+    ["wh_main_western_sylvania_castle_templehof"] = {652,443}, 
+    ["wh_main_western_sylvania_fort_oberstyre"] = {634,436}, 
+    ["wh_main_western_sylvania_schwartzhafen"] = {641,394},
+    ["wh_main_wissenland_wissenburg"] = {531,394}, 
+    ["wh_main_wissenland_nuln"] = {524,410}, 
+    ["wh_main_wissenland_pfeildorf"] = {563,379}, 
+    ["wh_main_reikland_grunburg"] = {514,427}, 
+    ["wh_main_reikland_altdorf"] = {496,450}, 
+    ["wh_main_reikland_helmgart"] = {466,428}, 
+    ["wh_main_reikland_eilhart"] = {445,450}, 
+    ["wh_main_the_wasteland_marienburg"] = {428,462}, 
+    ["wh_main_the_wasteland_gorssel"] = {432,485}, 
+    ["wh_main_forest_of_arden_gisoreux"] = {413,442}, 
+    ["wh_main_forest_of_arden_castle_artois"] = {389,451}, 
+    ["wh_main_couronne_et_languille_languille"] = {363,470}, 
+    ["wh_main_couronne_et_languille_couronne"] = {384,491}, 
+    ["wh_main_lyonesse_lyonesse"] = {340,440}, 
+    ["wh_main_lyonesse_mousillon"] = {356,420}, 
+    ["wh_main_bordeleaux_et_aquitaine_bordeleaux"] = {367,396}, 
+    ["wh_main_bordeleaux_et_aquitaine_aquitaine"] = {384,380}, 
+    ["wh_main_bastonne_et_montfort_castle_bastonne"] = {404,407}, 
+    ["wh_main_bastonne_et_montfort_montfort"] = {438,401}, 
+    ["wh_main_northern_grey_mountains_karak_ziflin"] = {441,413}, 
+    ["wh_main_northern_grey_mountains_grung_zint"] = {407,466}, 
+    ["wh_main_northern_grey_mountains_blackstone_post"] = {422,428}, 
+    ["wh_main_parravon_et_quenelles_parravon"] = {450,379}, 
+    ["wh_main_parravon_et_quenelles_quenelles"] = {444,349}, 
+    ["wh_main_carcassone_et_brionne_castle_carcassonne"] = {431,318}, 
+    ["wh_main_carcassone_et_brionne_brionne"] = {399,339}, 
+    ["wh2_main_yvresse_elessaeli"] = {270,292}, 
+    ["wh2_main_yvresse_tor_yvresse"] = {288,347}, 
+    ["wh2_main_cothique_tor_koruali"] = {284,394}, 
+    ["wh2_main_cothique_mistnar"] = {290,417}, 
+    ["wh2_main_chrace_elisia"] = {261,417}, 
+    ["wh2_main_chrace_tor_achare"] = {247,412}, 
+    ["wh2_main_nagarythe_shrine_of_khaine"] = {214,447}, 
+    ["wh2_main_nagarythe_tor_anlec"] = {197,423}, 
+    ["wh2_main_nagarythe_tor_dranil"] = {166,401}, 
+    ["wh2_main_unicorn_gate"] = {177,395}, 
+    ["wh2_main_phoenix_gate"] = {208,406}, 
+    ["wh2_main_griffon_gate"] = {154,371}, 
+    ["wh2_main_eagle_gate"] = {148,343}, 
+    ["wh2_main_tiranoc_tor_anroc"] = {145,341}, 
+    ["wh2_main_tiranoc_whitepeak"] = {147,319}, 
+    ["wh2_main_caledor_tor_sethai"] = {162,298}, 
+    ["wh2_main_caledor_vauls_anvil"] = {174,274}, 
+    ["wh2_main_eataine_tower_of_lysean"] = {198,291}, 
+    ["wh2_main_eataine_lothern"] = {211,279}, 
+    ["wh2_main_eataine_angerrial"] = {237,289}, 
+    ["wh2_main_eataine_shrine_of_asuryan"] = {226,306}, 
+    ["wh2_main_saphery_port_elistor"] = {253,305}, 
+    ["wh2_main_saphery_tower_of_hoeth"] = {258,327}, 
+    ["wh2_main_saphery_tor_finu"] = {261,352}, 
+    ["wh2_main_avelorn_tor_saroir"] = {238,370}, 
+    ["wh2_main_avelorn_evershale"] = {207,377}, 
+    ["wh2_main_avelorn_gaean_vale"] = {226,361},
+    ["wh2_main_ellyrion_whitefire_tor"] = {175,358}, 
+    ["wh2_main_ellyrion_tor_elyr"] = {171,334}, 
+    ["wh2_main_aghol_wastelands_palace_of_princes"] = {373,696}, 
+    ["wh2_main_aghol_wastelands_fortress_of_the_damned"] = {337,687},
+    ["wh2_main_aghol_wastelands_the_palace_of_ruin"] = {252,697},  
+    ["wh2_main_deadwood_shagrath"] = {291,670}, 
+    ["wh2_main_deadwood_the_frozen_city"] = {261,684}, 
+    ["wh2_main_deadwood_dargoth"] = {224,659}, 
+    ["wh2_main_deadwood_nagrar"] = {231,648}, 
+    ["wh2_main_the_road_of_skulls_the_black_pillar"] = {187,647}, 
+    ["wh2_main_the_road_of_skulls_kauark"] = {177,666}, 
+    ["wh2_main_the_road_of_skulls_spite_reach"] = {155,653}, 
+    ["wh2_main_the_road_of_skulls_har_ganeth"] = {143,632}, 
+    ["wh2_main_the_chill_road_ghrond"] = {130,647}, 
+    ["wh2_main_the_chill_road_ashrak"] = {104,668}, 
+    ["wh2_main_the_chill_road_the_great_arena"] = {96,640}, 
+    ["wh2_main_iron_mountains_naggarond"] = {84,630}, 
+    ["wh2_main_iron_mountains_har_kaldra"] = {57,652}, 
+    ["wh2_main_iron_mountains_altar_of_ultimate_darkness"] = {21,646}, 
+    ["wh2_main_iron_mountains_rackdo_gorge"] = {28,629}, 
+    ["wh2_main_the_black_flood_temple_of_khaine"] = {57,605}, 
+    ["wh2_main_the_black_flood_cragroth_deep"] = {81,577}, 
+    ["wh2_main_the_black_flood_hag_graef"] = {104,593}, 
+    ["wh2_main_the_black_flood_shroktak_mount"] = {29,585}, 
+    ["wh2_main_obsidian_peaks_circle_of_destruction"] = {122,560}, 
+    ["wh2_main_obsidian_peaks_clar_karond"] = {95,534}, 
+    ["wh2_main_obsidian_peaks_venom_glade"] = {119,522}, 
+    ["wh2_main_obsidian_peaks_storag_kor"] = {50,538}, 
+    ["wh2_main_the_clawed_coast_hoteks_column"] = {143,535}, 
+    ["wh2_main_the_clawed_coast_the_monoliths"] = {175,543}, 
+    ["wh2_main_the_clawed_coast_the_twisted_glade"] = {177,509}, 
+    ["wh2_main_the_broken_land_blacklight_tower"] = {177,592}, 
+    ["wh2_main_the_broken_land_slavers_point"] = {212,570}, 
+    ["wh2_main_the_broken_land_karond_kar"] = {242,605}, 
+    ["wh2_main_doom_glades_vauls_anvil"] = {90,483}, 
+    ["wh2_main_doom_glades_hag_hall"] = {73,460}, 
+    ["wh2_main_doom_glades_ice_rock_gorge"] = {50,453}, 
+    ["wh2_main_doom_glades_temple_of_addaioth"] = {54,497}, 
+    ["wh2_main_blackspine_mountains_red_desert"] = {11,560}, 
+    ["wh2_main_blackspine_mountains_plain_of_spiders"] = {16,508}, 
+    ["wh2_main_blackspine_mountains_plain_of_dogs"] = {11,437}, 
+    ["wh2_main_the_black_coast_bleak_hold_fortress"] = {87,415}, 
+    ["wh2_main_the_black_coast_arnheim"] = {98,379}, 
+    ["wh2_main_titan_peaks_ancient_city_of_quintex"] = {47,388}, 
+    ["wh2_main_titan_peaks_the_moon_shard"] = {71,357}, 
+    ["wh2_main_titan_peaks_ssildra_tor"] = {36,354}, 
+    ["wh2_main_titan_peaks_ironspike"] = {21,356}, 
+    ["wh2_main_isthmus_of_lustria_ziggurat_of_dawn"] = {70,325}, 
+    ["wh2_main_isthmus_of_lustria_skeggi"] = {90,327}, 
+    ["wh2_main_isthmus_of_lustria_hexoatl"] = {48,293}, 
+    ["wh2_main_isthmus_of_lustria_fallen_gates"] = {21,317}
+} --:map<string, {number, number}>
 
 
 
@@ -142,6 +302,10 @@ end;
 
 --v function(region: CA_REGION) --> boolean
 function region_has_portal(region)
+    if DF_SPAWN_LOCATIONS[region:name()] == nil then
+        GOCLOG("Region with portal ["..region:name().."] doesn't have a set up spawn location!")
+        return false
+    end
     for i = 1, #DF_CHAOS_PORTAL_BUILDING do
         if region:building_exists(DF_CHAOS_PORTAL_BUILDING[i]) then
             GOCLOG("Region has portal returning true for region ["..region:name().."] ")
@@ -167,12 +331,13 @@ local function spawn_chaos(region)
         DF_CHAOS_ARMY_FACTION,
         unit_string,
         region:name(),
-        region:settlement():logical_position_x() + 2,
-        region:settlement():logical_position_y() + 2,
+        DF_SPAWN_LOCATIONS[region:name()][1],
+        DF_SPAWN_LOCATIONS[region:name()][2],
         true,
         true,
         function(cqi)
             GOCLOG("Spawned a chaos army at ["..region:name().."] sucessfully with cqi ["..tostring(cqi).."]")
+            cm:apply_effect_bundle_to_characters_force("wh_main_bundle_military_upkeep_free_force", cqi, 0, true);
             cm:force_diplomacy("faction:"..DF_CHAOS_ARMY_FACTION, "faction:wh_main_chs_chaos", "war", false, false, false)
             if cm:get_faction("wh_main_chs_chaos"):is_dead() == false and cm:get_faction(DF_CHAOS_ARMY_FACTION):is_vassal_of(cm:get_faction("wh_main_chs_chaos")) == false then
                 GOCLOG("Making the spawned faction a vassal!")
@@ -246,3 +411,40 @@ core:add_listener(
         end
     end,
     true)
+
+
+--[[ testing code
+--v function(text: string)
+function WRITE(text)
+    if not __write_output_to_logfile then
+        return; 
+    end
+
+    local logText = tostring(text)
+    local popLog = io.open("coordinates.txt","a")
+    popLog :write(logText)
+    popLog :flush()
+    popLog :close()
+end
+
+
+core:add_listener(
+    "WritingDownShit",
+    "ShortcutTriggered",
+    function(context) return context.string == "camera_bookmark_view0"; end, --default F9
+    function(context)
+        if not cm:get_saved_value("wars_declared_testing") then
+            for i = 0, cm:model():world():faction_list():num_items() - 1 do
+                if not cm:model():world():faction_list():item_at(i):is_dead() or cm:model():world():faction_list():item_at(i):name() == "wh_main_chs_chaos" then
+                    cm:force_declare_war(cm:model():world():faction_list():item_at(i):name(), "wh_main_chs_chaos", false, false)
+                    cm:set_saved_value("wars_declared_testing", true)
+                end
+            end
+        end
+
+        local character = cm:get_faction("wh_main_chs_chaos"):faction_leader()
+        WRITE("[\""..character:region():name().." \"] = {"..character:logical_position_x()..","..character:logical_position_y().."}, \n")
+    end,
+    true
+)
+--]]
